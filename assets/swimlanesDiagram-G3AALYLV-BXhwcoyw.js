45376a835a6b63178e771064bdf5a45349b9a53d
keshavmishra27 <webflask27@gmail.com>
2026-07-09 13:58:17 +0530

    Updates

--- assets/swimlanesDiagram-G3AALYLV-BXhwcoyw.js
+++
@@ -1,8 +0,0 @@
-import"./src-DVPOk4iJ.js";import"./chunk-WYO6CB5R-BCeyyiIG.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-ICXQ74PX-CK5AnJSq.js";import"./chunk-C7G6YPKG-Tc2_V6DB.js";import"./chunk-OGEWGWER-CVDqTB0w.js";import"./chunk-HOUHSVGY-bXg_xeY0.js";import"./chunk-Q4XR5HBZ-CWJ68Vqv.js";import"./chunk-ZGVPDNZ5-C7A9IskO.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./index-CFent0aI.js";import"./chunk-32BRIVSS-BxhKEMV7.js";import"./chunk-XXDRQBXY-CYB39NOj.js";import"./chunk-VR4S4FIN-CUBvxwXY.js";import{r as t,t as n}from"./chunk-PUDLZKDR-CdJP7goL.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
